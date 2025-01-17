a7db556595fdf5aadc3dbe650e8006990d3d74e2
Ahamed Favas <ahamedfavasedanatt@gmail.com>
2025-01-18 00:21:26 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,28 @@
  */
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+    async fetch(response, env, ctx) {
+  async function sendEmail(textToSend) {
+    try {
+        const response = await fetch(`https://api.mailgun.net/v3/${env.mailGunDomain}/messages`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": `Basic ${btoa(`api:${env.mailGunAPIKEY}`)}`
+            },
+            body: new URLSearchParams({
+                from: env.mailGunFromAddress,
+                to: env.mailGunToAddress,
+                subject: "Your Website is Offline",
+                text: textToSend
+            })
+        });
+        return response;
+    } catch (error) {
+        return false;
+    }
+	}
+	const resp = await sendEmail("Helllllo");
+    return new Response(await resp.text())    
 	},
-};
+}
