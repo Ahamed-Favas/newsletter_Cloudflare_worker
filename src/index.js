20747394f2de08b3dd8ee0f068e171e71ae76cc1
Ahamed Favas <ahamedfavasedanatt@gmail.com>
2025-01-21 01:33:25 +0530

    miles to go before i sleep

--- src/index.js
+++ src/index.js
@@ -1,23 +1,63 @@
 import { sendEmail } from "./services/mailer";
 import { parseFeed } from "./actions/parseRss";
 
-const url = ""
+const timesofindia_topstories = "https://timesofindia.indiatimes.com/rssfeedstopstories.cms";
+
 export default {
   async fetch(request, env, ctx) {
-    return new Response("Worker is running. Scheduled tasks will run automatically.");
+    if (request.method === "POST") {
+      const validUsername = env.HttpUsername;
+      const validPassword = env.HttpPassword;
+
+      const authHeader = request.headers.get('authorization');
+      if (!authHeader || !authHeader.startsWith("Basic ")) {
+          return new Response("Unauthorized", { status: 401, headers: { "WWW-Authenticate": "Basic" } });
+      }
+
+      const encodedCredentials = authHeader.replace("Basic ", "");
+      const decodedCredentials = atob(encodedCredentials);
+      const [username, password] = decodedCredentials.split(':');
+
+      if (username !== validUsername || password !== validPassword) {
+      return new Response("Forbidden", { status: 403 });
+      }
+      const body = await request.json();
+      const userEmail = body.user_email;
+      const userFeed = body.user_feed;
+      if (!userEmail || !userFeed) {
+        return new Response("Missing params in request body", { status: 400 });
+      }
+      if ( userFeed === "timesofindia_topstories" ) {
+        // Task to do mailing, after fetching from db and done processing by ai 
+      }
+    return new Response("user is validated")
+    }
+    return new Response("method is not allowed", { status: 405 })
   },
 
   async scheduled(event, env, ctx) {
     console.log("Running scheduled task at:", event.cron);
-    try {
-      ctx.waitUntil(handleScheduledTask(env));
-    } catch (error) {
-      console.error("Scheduled task failed:", error);
+    switch(event.cron) {
+      case "0 */6 * * *":
+        try {
+          ctx.waitUntil(handleScheduledTask(env));
+        } catch (error) {
+          console.error("Scheduled task failed:", error);
+        }
+        break;
+      case "0 0 */3 * *":
+        try {
+          // do scheduled deletion of d1 db
+        } catch (error) {
+          console.error("Scheduled task failed:", error);
+        }
+        break;
     }
   }
 };
 
 async function handleScheduledTask(env) {
+  // do scheduled task, ie getting content in every 6 hr.
   // async function getGoodMorningMessage(retries = 3) {
     
   //   for (let i = 0; i < retries; i++) {
@@ -36,7 +76,7 @@ async function handleScheduledTask(env) {
 
   try {
     // const goodMorningMessage = await getGoodMorningMessage();
-    const feedContent =  await parseFeed();
+    const feedContent =  await parseFeed([timesofindia_topstories], env);
     const emailHtml = await generateEmail(feedContent);
     const emailResponse = await sendEmail(emailHtml);
 
@@ -45,4 +85,5 @@ async function handleScheduledTask(env) {
     console.error("Task execution failed:", error);
     return false;
   }
+  return
 }
